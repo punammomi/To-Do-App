ab057775cf721610c835daae45cefb628355e300
Punam <159411425+punammomi@users.noreply.github.com>
2026-06-05 08:47:47 +0600

    useState use lazy initializer which is in the bracket.This initializer fetch the data from local storage.If want to update data or add new task use setTask.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
 import { useEffect, useMemo, useState } from "react";
 import {Check, ClipboardList, Plus, Trash2} from "lucide-react";
 import "./styles/App.css";
+
+const Store = "Work List";
+
+function App() {
+    const [tasks, setTasks] = useState(() => {
+        const storedTasks = localStorage.getItem(Store);
+        return storedTasks ? JSON.parse(storedTasks) : [];
+    }
+}
